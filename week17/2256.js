a548d3cf9dcbee76265745c177f6b027597c94d6
Sabrina <sabrina@innoagile.com.tw>
2024-04-13 18:09:25 +0800

    problem 2256 solution

--- week17/2256.js
+++ week17/2256.js
@@ -2,4 +2,38 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minimumAverageDifference = function (nums) {};
+var minimumAverageDifference = function (nums) {
+  let leftSum = 0,
+    totalSum = 0,
+    index = 0;
+  let minDiff = Infinity;
+  let length = nums.length;
+
+  // cal total sum
+  for (let i = 0; i < length; i++) {
+    totalSum += nums[i];
+  }
+
+  for (let i = 0; i < length; i++) {
+    // cal left sum and avg
+    leftSum += nums[i];
+    let leftCount = i + 1;
+    let leftAvg = Math.floor(leftSum / leftCount);
+
+    // cal right sum and avg
+    let rightSum = totalSum - leftSum;
+    let rightCount = length - leftCount;
+    // handle last round when divisor is zero
+    let rightAvg = Math.floor(rightSum / (i == length - 1 ? 1 : rightCount));
+
+    // cal absolute difference
+    let currDiff = Math.abs(leftAvg - rightAvg);
+
+    // if current diff < min diff, replace diff and index
+    if (currDiff < minDiff) {
+      minDiff = currDiff;
+      index = i;
+    }
+  }
+  return index;
+};
