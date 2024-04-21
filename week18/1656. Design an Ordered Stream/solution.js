d80660d2ccdf5193867eea7079d37ee4e9684bfa
Sabrina <sabrina@innoagile.com.tw>
2024-04-21 10:31:13 +0800

    update 1656 solution

--- week18/1656. Design an Ordered Stream/solution.js
+++ week18/1656. Design an Ordered Stream/solution.js
@@ -1,14 +1,27 @@
 /**
  * @param {number} n
  */
-var OrderedStream = function (n) {};
+var OrderedStream = function (n) {
+  this.pointer = 0;
+  this.stream = [];
+};
 
 /**
  * @param {number} idKey
  * @param {string} value
  * @return {string[]}
  */
-OrderedStream.prototype.insert = function (idKey, value) {};
+OrderedStream.prototype.insert = function (idKey, value) {
+  const chunk = [];
+  this.stream[idKey - 1] = value;
+
+  while (this.stream[this.pointer]) {
+    chunk.push(this.stream[this.pointer]);
+    this.pointer++;
+  }
+
+  return chunk;
+};
 
 /**
  * Your OrderedStream object will be instantiated and called as such:
