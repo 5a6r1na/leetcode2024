5e3cb876b8eae19995760edb55aa21755a91a87f
Sabrina <chiawenli.1219@gmail.com>
2024-05-19 12:31:45 +0800

    [MODIFY] 2059 solution

--- week20/2059. Minimum Operations to Convert Number/solution.js
+++ week20/2059. Minimum Operations to Convert Number/solution.js
@@ -1,30 +1,52 @@
 /**
- * @param {number} n
+ * @param {number[]} nums
+ * @param {number} start
+ * @param {number} goal
+ * @return {number}
  */
-var OrderedStream = function (n) {
-  this.pointer = 0;
-  this.stream = [];
-};
+var minimumOperations = function (nums, start, goal) {
+  // define queue
+  const q = [];
+  // track visited number
+  const seen = new Set();
+  // set queue starting number and step count
+  q.push([start, 0]);
+  // first visited number
+  seen.add(start);
+  // track current queue position
+  let front = 0;
 
-/**
- * @param {number} idKey
- * @param {string} value
- * @return {string[]}
- */
-OrderedStream.prototype.insert = function (idKey, value) {
-  const chunk = [];
-  this.stream[idKey - 1] = value;
+  // operations
+  const operations = [(n, m) => n + m, (n, m) => n - m, (n, m) => n ^ m];
 
-  while (this.stream[this.pointer]) {
-    chunk.push(this.stream[this.pointer]);
-    this.pointer++;
+  // while-loop: still elements unvisited
+  while (front < q.length) {
+    //
+    let length = q.length - front;
+
+    while (length > 0) {
+      let [num, steps] = q[front];
+      front++;
+      length--;
+
+      for (const operation of operations) {
+        for (const n of nums) {
+          // calculate a newNem with all operations
+          const newNum = operation(num, n);
+
+          // reached goal, calculate the steps taken, operation to reach goal is an additional step
+          if (newNum === goal) {
+            return steps + 1;
+          }
+          // If newNum is within valid range and not seen before, add to the queue and marked seen.
+          if (0 <= newNum && newNum <= 1000 && !seen.has(newNum)) {
+            seen.add(newNum);
+            q.push([newNum, steps + 1]);
+          }
+        }
+      }
+    }
   }
-
-  return chunk;
+  // if no finding of goal
+  return -1;
 };
-
-/**
- * Your OrderedStream object will be instantiated and called as such:
- * var obj = new OrderedStream(n)
- * var param_1 = obj.insert(idKey,value)
- */
