83c553e12c5fa3a2260a4058bf0858518d134938
Sabrina Li <chiawenli.1219@gmail.com>
2024-07-14 12:35:10 +0800

    [MODIFY] 1022 solution

--- week23/1022. Sum of Root To Leaf Binary Numbers/solution.js
+++ week23/1022. Sum of Root To Leaf Binary Numbers/solution.js
@@ -1,17 +1,40 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @param {number} start
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val);
+ *     this.left = (left===undefined ? null : left);
+ *     this.right = (right===undefined ? null : right);
+ * }
+ */
+/**
+ * @param {TreeNode} root
  * @return {number}
  */
-var getMinDistance = function (nums, target, start) {
-  let array = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      let res = Math.abs(i - start);
-      array.push(res);
+var sumRootToLeaf = function (root) {
+  if (!root) return 0;
+
+  const dfs = (node, currentPath) => {
+    currentPath += node.val;
+
+    // Check leaf node
+    if (!node.left && !node.right) {
+      // Convert binary string
+      return parseInt(currentPath, 2);
     }
-  }
-  // spread operator
-  return Math.min(...array);
+
+    let sum = 0;
+    // Left subtree
+    if (node.left) {
+      sum += dfs(node.left, currentPath);
+    }
+    // Right subtree
+    if (node.right) {
+      sum += dfs(node.right, currentPath);
+    }
+
+    return sum;
+  };
+
+  // Start DFS from root with empty path
+  return dfs(root, "");
 };
