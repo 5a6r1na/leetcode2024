309e691e55867e557e505c39a82179bf2261e508
Sabrina Li <chiawenli.1219@gmail.com>
2024-07-28 16:07:31 +0800

    [MODIFY] add comments

--- week25/57. Insert Interval/solution.js
+++ week25/57. Insert Interval/solution.js
@@ -7,14 +7,16 @@ var insert = function (intervals, newInterval) {
   const result = [];
   let currMin = newInterval[0];
   let currMax = newInterval[1];
-  let small = 0;
+  let index = 0;
 
+  // [CASE]: no overlaps
   for (let i = 0; i < intervals.length; i++) {
     if (intervals[i][1] < newInterval[0]) {
       result.push(intervals[i]);
-      small++;
+      index++;
     } else if (intervals[i][0] > newInterval[1]) {
       result.push(intervals[i]);
+      // [CASE]: with overlaps
     } else if (intervals[i][0] <= newInterval[0]) {
       if (intervals[i][1] >= newInterval[1]) {
         currMax = intervals[i][1];
@@ -24,6 +26,7 @@ var insert = function (intervals, newInterval) {
       currMax = intervals[i][1];
     }
   }
-  result.splice(small, 0, [currMin, currMax]);
+  // Insert to result array with at specific index
+  result.splice(index, 0, [currMin, currMax]);
   return result;
 };
