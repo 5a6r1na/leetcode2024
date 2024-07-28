e85592b85fcf3c76fd79396e45fbae9eca72bfb9
Sabrina Li <chiawenli.1219@gmail.com>
2024-07-28 19:51:22 +0800

    [ADD] week26 files

---
+++ week26/238. Product of Array Except Self/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  let result = [];
+  let prefix = [];
+  let suffix = [];
+  let length = nums.length;
+
+  prefix[0] = 1;
+  suffix[length - 1] = 1;
+  // [STEP]: calculate prefix
+  for (let i = 1; i < length; i++) {
+    prefix[i] = nums[i - 1] * prefix[i - 1];
+  }
+
+  // [STEP]: calculate suffix
+  for (let i = length - 2; i >= 0; i--) {
+    suffix[i] = nums[i + 1] * suffix[i + 1];
+  }
+
+  // [STEP]: calculate result
+  for (let i = 0; i < length; i++) {
+    result.push(prefix[i] * suffix[i]);
+  }
+  return result;
+};
