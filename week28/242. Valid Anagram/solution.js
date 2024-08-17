718bcf2f67cd6bc4928dbb245821c201e5cd0a03
Sabrina Li <chiawenli.1219@gmail.com>
2024-08-17 21:39:39 +0800

    [MODIFY] solution for problem 242

--- week28/242. Valid Anagram/solution.js
+++ week28/242. Valid Anagram/solution.js
@@ -3,4 +3,29 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function (s, t) {};
+var isAnagram = function (s, t) {
+  let sLen = s.length;
+  let tLen = t.length;
+  let objS = {};
+  let objT = {};
+
+  // [CASE]: s and t are diff length
+  if (sLen !== tLen) {
+    return false;
+  }
+
+  // [STEP]: Construct hashmap
+  for (let i = 0; i < sLen; i++) {
+    objS[s[i]] = objS[s[i]] ? objS[s[i]] + 1 : 1;
+    objT[t[i]] = objT[t[i]] ? objT[t[i]] + 1 : 1;
+  }
+
+  // [STEP]: For each key in objS check objK
+  for (const key in objS) {
+    // [CASE]: Key in objS does NOT exist in objK
+    if (objS[key] != objT[key]) {
+      return false;
+    }
+  }
+  return true;
+};
