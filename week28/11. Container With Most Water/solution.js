d2c1443755ee6f2a60d4060437625e5c1f2496b1
Sabrina Li <chiawenli.1219@gmail.com>
2024-08-18 13:43:33 +0800

    [MODIFY] solution for problem 11

--- week28/11. Container With Most Water/solution.js
+++ week28/11. Container With Most Water/solution.js
@@ -2,4 +2,24 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function (height) {};
+var maxArea = function (height) {
+  let leftPointer = 0;
+  let rightPointer = height.length - 1;
+  let result = 0;
+
+  while (leftPointer < rightPointer) {
+    // [STEP]: Calculate current pointer area
+    let area =
+      (rightPointer - leftPointer) *
+      Math.min(height[leftPointer], height[rightPointer]);
+    // [STEP]: Compare and replace for max area
+    result = Math.max(result, area);
+    // [CASE]: Move smaller pointer to increase chance of larger result
+    if (height[leftPointer] < height[rightPointer]) {
+      leftPointer++;
+    } else {
+      rightPointer--;
+    }
+  }
+  return result;
+};
