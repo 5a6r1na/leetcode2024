8756db8a644c7fdda88717bc5950bf43d1a07fb0
5a6r1na <chiawenli.1219@gmail.com>
2025-03-06 17:08:13 +0800

    [MODIFY] added comment to solution

--- week25/121. Best Time to Buy and Sell Stock/solution.js
+++ week25/121. Best Time to Buy and Sell Stock/solution.js
@@ -3,17 +3,25 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
+  // [DEFINE]: Initial profit
   let profit = 0;
+  // [DEFINE]: Initial buyPrice
   let buyPrice = prices[0];
 
   for (let i = 1; i < prices.length; i++) {
-    if (buyPrice > prices[i]) {
-      buyPrice = prices[i];
-    }
-    let currProfit = prices[i] - buyPrice;
-    if (currProfit > profit) {
-      profit = currProfit;
-    }
+      // [CASE]: If current buyPrice is greater than the current price, update
+      if (buyPrice > prices[i]) {
+          // [STEP]: Update buyPrice to the lower price
+          buyPrice = prices[i]
+      }
+
+      // [DEFINE]: Current profit
+      let current = prices[i] - buyPrice;
+      // [CASE]: If current profit it greater than record profit
+      if (current > profit) {
+          // [STEP]: Update record profit to current profit
+          profit = current;
+      }
   }
   return profit;
 };
